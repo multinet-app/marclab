fa337b465a8eac251ecd715f96262d11e902b762
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-07-09 16:34:51 -0400

    Use requests session for token auth

--- multinet.py
+++ multinet.py
@@ -16,17 +16,23 @@ NETWORK_NAME = "network"
 
 def main():
     if len(sys.argv) < 3:
-        print("usage: multinet.py <instance-url> <workspace>", file=sys.stderr)
+        print(
+            "usage: multinet.py <instance-url> <workspace> <api-token>", file=sys.stderr
+        )
         return 1
 
     # Extract args
-    _, base_url, workspace = sys.argv
+    _, base_url, workspace, api_token = sys.argv
+
+    # Inject auth token into every request
+    api_client = requests.Session()
+    api_client.headers.update({"Authorization": f"Token {api_token}"})
 
     # Delete network and tables if they exist
     workspace_url = f"{base_url}/api/workspaces/{workspace}"
-    requests.delete(f"{workspace_url}/networks/{NETWORK_NAME}")
-    requests.delete(f"{workspace_url}/tables/{NODE_TABLE_NAME}")
-    requests.delete(f"{workspace_url}/tables/{EDGE_TABLE_NAME}")
+    api_client.delete(f"{workspace_url}/networks/{NETWORK_NAME}")
+    api_client.delete(f"{workspace_url}/tables/{NODE_TABLE_NAME}")
+    api_client.delete(f"{workspace_url}/tables/{EDGE_TABLE_NAME}")
 
 
 if __name__ == "__main__":
