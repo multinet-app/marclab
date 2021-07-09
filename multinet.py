6553480305dcb8b9745bf4aca29cb46f4c0459b2
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-07-09 17:47:57 -0400

    Add complete network creation

--- multinet.py
+++ multinet.py
@@ -1,17 +1,42 @@
-import json
+import shutil
+import tempfile
 import requests
 import sys
 import csv
-import pandas as pd
-import copy
-import bisect
-from typing import Any, List, TypedDict, Optional, cast
+from requests.models import Response
+from s3_file_field_client import S3FileFieldClient
 
 NODE_TABLE_NAME = "nodes"
 EDGE_TABLE_NAME = "links"
 
 # TODO: Change to something better
 NETWORK_NAME = "network"
+
+
+def raise_for_status(r: Response):
+    """A wrapper for repsonse.raise_for_status."""
+    try:
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        print(error.response.text)
+        raise error
+
+
+def fix_links_csv():
+    outfile = tempfile.NamedTemporaryFile(mode="w", delete=False)
+    with open("links.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames)
+        writer.writeheader()
+
+        for row in reader:
+            if len(row["_from"].split("/")) != 2:
+                row["_from"] = f"{NODE_TABLE_NAME}/{row['_from']}"
+            if len(row["_to"].split("/")) != 2:
+                row["_to"] = f"{NODE_TABLE_NAME}/{row['_to']}"
+            writer.writerow(row)
+
+    shutil.move(outfile.name, "links.csv")
 
 
 def main():
@@ -30,9 +55,56 @@ def main():
 
     # Delete network and tables if they exist
     workspace_url = f"{base_url}/api/workspaces/{workspace}"
-    api_client.delete(f"{workspace_url}/networks/{NETWORK_NAME}")
-    api_client.delete(f"{workspace_url}/tables/{NODE_TABLE_NAME}")
-    api_client.delete(f"{workspace_url}/tables/{EDGE_TABLE_NAME}")
+    api_client.delete(f"{workspace_url}/networks/{NETWORK_NAME}/")
+    api_client.delete(f"{workspace_url}/tables/{NODE_TABLE_NAME}/")
+    api_client.delete(f"{workspace_url}/tables/{EDGE_TABLE_NAME}/")
+
+    s3ff_client = S3FileFieldClient(f"{base_url}/api/s3-upload/", api_client)
+
+    # Upload nodes.csv
+    with open("./nodes.csv") as file_stream:
+        field_value = s3ff_client.upload_file(
+            file_stream, "nodes.csv", "api.Upload.blob"
+        )["field_value"]
+
+    raise_for_status(
+        api_client.post(
+            f"{workspace_url}/uploads/csv/",
+            json={
+                "field_value": field_value,
+                "edge": False,
+                "table_name": NODE_TABLE_NAME,
+                # TODO: Maybe place columns field here
+            },
+        )
+    )
+
+    # Upload links.csv
+    fix_links_csv()
+    with open("./links.csv") as file_stream:
+        field_value = s3ff_client.upload_file(
+            file_stream, "links.csv", "api.Upload.blob"
+        )["field_value"]
+
+    raise_for_status(
+        api_client.post(
+            f"{workspace_url}/uploads/csv/",
+            json={
+                "field_value": field_value,
+                "edge": True,
+                "table_name": EDGE_TABLE_NAME,
+                # TODO: Maybe place columns field here
+            },
+        )
+    )
+
+    # Create network
+    raise_for_status(
+        api_client.post(
+            f"{workspace_url}/networks/",
+            json={"name": NETWORK_NAME, "edge_table": EDGE_TABLE_NAME},
+        )
+    )
 
 
 if __name__ == "__main__":
