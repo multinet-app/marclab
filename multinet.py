a9c7190bb69b4dada1d45612040730e9a6ebeefb
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-07-09 16:23:14 -0400

    Simplify code

--- multinet.py
+++ multinet.py
@@ -7,11 +7,12 @@ import copy
 import bisect
 from typing import Any, List, TypedDict, Optional, cast
 
-NODE_TABLE_NAME = 'nodes'
-EDGE_TABLE_NAME = 'links'
+NODE_TABLE_NAME = "nodes"
+EDGE_TABLE_NAME = "links"
 
 # TODO: Change to something better
-NETWORK_NAME = 'network'
+NETWORK_NAME = "network"
+
 
 def main():
     if len(sys.argv) < 3:
@@ -21,24 +22,11 @@ def main():
     # Extract args
     _, base_url, workspace = sys.argv
 
-    # Delete network if it exists
-    network_url = f'{base_url}/api/workspaces/{workspace}/graphs/{NETWORK_NAME}'
-    r = requests.get(network_url)
-    if r.status_code != 404:
-        requests.delete(network_url)
-
-    # Delete node table if it exists
-    node_table_url = f'{base_url}/api/workspaces/{workspace}/tables/{NODE_TABLE_NAME}'
-    r = requests.get(node_table_url)
-    if r.status_code != 404:
-        requests.delete(node_table_url)
-
-    # Delete edge table if it exists
-    edge_table_url = f'{base_url}/api/workspaces/{workspace}/tables/{EDGE_TABLE_NAME}'
-    r = requests.get(edge_table_url)
-    if r.status_code != 404:
-        requests.delete(edge_table_url)
-
+    # Delete network and tables if they exist
+    workspace_url = f"{base_url}/api/workspaces/{workspace}"
+    requests.delete(f"{workspace_url}/networks/{NETWORK_NAME}")
+    requests.delete(f"{workspace_url}/tables/{NODE_TABLE_NAME}")
+    requests.delete(f"{workspace_url}/tables/{EDGE_TABLE_NAME}")
 
 
 if __name__ == "__main__":
