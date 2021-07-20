4ac9f4a874a02a58006418b788cda588954ad128
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-07-20 14:31:35 -0400

    Use BaseUrlSession, add wait for table uploads

--- multinet.py
+++ multinet.py
@@ -1,10 +1,12 @@
 import shutil
 import time
 import tempfile
-import requests
+from typing import Dict, List, Set
 import sys
 import csv
 from requests.models import Response
+from requests.exceptions import HTTPError
+from requests_toolbelt.sessions import BaseUrlSession
 from s3_file_field_client import S3FileFieldClient
 
 NODE_TABLE_NAME = "nodes"
@@ -18,7 +20,7 @@ def raise_for_status(r: Response):
     """A wrapper for repsonse.raise_for_status."""
     try:
         r.raise_for_status()
-    except requests.exceptions.HTTPError as error:
+    except HTTPError as error:
         print(error.response.text)
         raise error
 
@@ -40,6 +42,25 @@ def fix_links_csv():
     shutil.move(outfile.name, "links.csv")
 
 
+def await_tasks_finished(api_client: BaseUrlSession, tasks: List[Dict]):
+    tasks_set: Set[int] = {t["id"] for t in tasks}
+
+    while tasks_set:
+        time.sleep(0.1)
+        for task_id in list(tasks_set):
+            r = api_client.get(f"uploads/{task_id}")
+            raise_for_status(r)
+
+            if r.json()["status"] == "FAILED":
+                errors = r.json()["error_messages"]
+                raise Exception(
+                    f"Upload with Task ID {task_id} failed with errors: {errors}"
+                )
+
+            if r.json()["status"] == "FINISHED":
+                tasks_set.remove(task_id)
+
+
 def main():
     if len(sys.argv) < 3:
         print(
@@ -51,80 +72,84 @@ def main():
     _, base_url, workspace, api_token = sys.argv
 
     # Inject auth token into every request
-    api_client = requests.Session()
+    api_client = BaseUrlSession(base_url=base_url)
     api_client.headers.update({"Authorization": f"Token {api_token}"})
 
-    # Delete network and tables if they exist
-    workspace_url = f"{base_url}/api/workspaces/{workspace}"
-    api_client.delete(f"{workspace_url}/networks/{NETWORK_NAME}/")
-    api_client.delete(f"{workspace_url}/tables/{NODE_TABLE_NAME}/")
-    api_client.delete(f"{workspace_url}/tables/{EDGE_TABLE_NAME}/")
-
-    s3ff_client = S3FileFieldClient(f"{base_url}/api/s3-upload/", api_client)
+    # Upload all files to S3
+    s3ff_client = S3FileFieldClient(f"/api/s3-upload/", api_client)
 
     # Upload nodes.csv
     with open("./nodes.csv") as file_stream:
-        field_value = s3ff_client.upload_file(
+        nodes_field_value = s3ff_client.upload_file(
             file_stream, "nodes.csv", "api.Upload.blob"
         )["field_value"]
-
-    raise_for_status(
-        api_client.post(
-            f"{workspace_url}/uploads/csv/",
-            json={
-                "field_value": field_value,
-                "edge": False,
-                "table_name": NODE_TABLE_NAME,
-                "columns": {
-                    "TypeID": "category",
-                    "Verified": "boolean",
-                    "Confidence": "number",
-                    "ParentID": "category",
-                    "Created": "date",
-                    "LastModified": "date",
-                    "TypeLabel": "category",
-                    "Volume (nm^3)": "number",
-                    "MaxDimension": "number",
-                    "MinZ": "number",
-                    "MaxZ": "number",
-                },
-            },
-        )
-    )
 
     # Upload links.csv
     fix_links_csv()
     with open("./links.csv") as file_stream:
-        field_value = s3ff_client.upload_file(
+        links_field_value = s3ff_client.upload_file(
             file_stream, "links.csv", "api.Upload.blob"
         )["field_value"]
 
-    raise_for_status(
-        api_client.post(
-            f"{workspace_url}/uploads/csv/",
-            json={
-                "field_value": field_value,
-                "edge": True,
-                "table_name": EDGE_TABLE_NAME,
-                "columns": {
-                    "TotalChildren": "number",
-                    "LastModified": "date",
-                    "Bidirectional": "boolean",
-                    "Type": "category",
-                    "TotalSourceArea(nm^2)": "number",
-                    "TotalTargetArea(nm^2)": "number",
-                },
-            },
-        )
-    )
+    # Update base url, since only workspace endpoints are needed now
+    api_client.base_url = f"{base_url}/api/workspaces/{workspace}/"
 
-    # Sleep to ensure files are processed
-    time.sleep(1)
+    # Delete network and tables if they exist
+    api_client.delete(f"networks/{NETWORK_NAME}/")
+    api_client.delete(f"tables/{NODE_TABLE_NAME}/")
+    api_client.delete(f"tables/{EDGE_TABLE_NAME}/")
+
+    # Create nodes table
+    r = api_client.post(
+        f"uploads/csv/",
+        json={
+            "field_value": nodes_field_value,
+            "edge": False,
+            "table_name": NODE_TABLE_NAME,
+            "columns": {
+                "TypeID": "category",
+                "Verified": "boolean",
+                "Confidence": "number",
+                "ParentID": "category",
+                "Created": "date",
+                "LastModified": "date",
+                "TypeLabel": "category",
+                "Volume (nm^3)": "number",
+                "MaxDimension": "number",
+                "MinZ": "number",
+                "MaxZ": "number",
+            },
+        },
+    )
+    raise_for_status(r)
+    nodes_upload = r.json()
+
+    # Create links table
+    r = api_client.post(
+        f"uploads/csv/",
+        json={
+            "field_value": links_field_value,
+            "edge": True,
+            "table_name": EDGE_TABLE_NAME,
+            "columns": {
+                "TotalChildren": "number",
+                "LastModified": "date",
+                "Bidirectional": "boolean",
+                "Type": "category",
+                "TotalSourceArea(nm^2)": "number",
+                "TotalTargetArea(nm^2)": "number",
+            },
+        },
+    )
+    raise_for_status(r)
+    links_upload = r.json()
+
+    await_tasks_finished(api_client, [nodes_upload, links_upload])
 
     # Create network
     raise_for_status(
         api_client.post(
-            f"{workspace_url}/networks/",
+            f"networks/",
             json={"name": NETWORK_NAME, "edge_table": EDGE_TABLE_NAME},
         )
     )
